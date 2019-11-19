a7cba7a324db6f1d350cfaef404c784a9c87aa15
avrahm <me@avrahm.com>
2019-11-19 15:29:06 -0500

    Updates

---
+++ precache-manifest.58d0481fdc7c0ac749cbbe1f98f9c772.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dd37b4b147d6f54b7d77d3d0014c654",
+    "url": "/project-2-TBD/index.html"
+  },
+  {
+    "revision": "71818348239f197fe33d",
+    "url": "/project-2-TBD/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "7c332b86adce5a5958eb",
+    "url": "/project-2-TBD/static/css/main.87c16a30.chunk.css"
+  },
+  {
+    "revision": "71818348239f197fe33d",
+    "url": "/project-2-TBD/static/js/2.73a60680.chunk.js"
+  },
+  {
+    "revision": "7c332b86adce5a5958eb",
+    "url": "/project-2-TBD/static/js/main.a47e200e.chunk.js"
+  },
+  {
+    "revision": "7b1e4f41bc01b588c507",
+    "url": "/project-2-TBD/static/js/runtime-main.e79234d6.js"
+  }
+]);
